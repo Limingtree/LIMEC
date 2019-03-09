9567c2e3312f24fca809d62c4f0e4042f9bc00aa
Limingtree <48266227+Limingtree@users.noreply.github.com>
2019-03-08 22:07:53 -0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"69.10.51.166:8080",
-  //"server.LIMEC.com:8080",
+  //"69.10.51.166:22",
+  //"",
 };
 
 struct CheckpointData {
